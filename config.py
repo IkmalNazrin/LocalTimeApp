c56ddbfae31631f974ceaffa4885451b1e3c97b9
IkmalNazrin <ikmalnazrin256@gmail.com>
2024-07-07 00:00:24 +0800

    Fix SQLALCHEMY_DATABASE_URI setting

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('INTERNAL_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
